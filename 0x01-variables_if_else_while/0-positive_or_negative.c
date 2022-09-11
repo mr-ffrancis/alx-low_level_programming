12bb307930536eb282260c2b6a80972fe817c783
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 05:19:46 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**main function starts the code
-*assigns a randon number to variable n
-*check if the number is positive or negative and prints the result
+/*
+* main function starts the code
+* assigns a randon number to variable n
+* check if the number is positive or negative and prints the result
 *
 */
 
@@ -14,9 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n>0)
-		printf("%d is positive\n",n);
-	else if (n<0)
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n); 	
