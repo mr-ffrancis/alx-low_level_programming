cfbea2094349c65b05b439cae5206ba83c940843
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 06:37:08 -0700

    commited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * assigns a randon number to variable n
+ * checks if the number is positive or negative and prints the result
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int n = 
+	char alp[] =  "abcdefghijklmnopqrstuvwxyz";
+
+	do
+	{
+
+	putchar(alp[n];
+	n++;		
+			
+	} while(n < strlen(alp[[]); 
+	return (0);
+}
