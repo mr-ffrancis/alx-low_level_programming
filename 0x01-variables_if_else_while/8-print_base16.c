393e63d3d10f6fb9698b7848d0559cb121432b50
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 10:20:26 -0700

    commited

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * assigns a randon number to variable n
+ * checks if the number is positive or negative and prints the result
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a < 103; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
