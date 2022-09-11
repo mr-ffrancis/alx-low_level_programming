3cf43b224cf7f1d4da4b33a75ed05358080daabb
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 02:21:43 -0700

    commited

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Put All the sizes of integers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: "& sizeof(char)&"\n");
+	printf("Size of a int: "& sizeof(int)&"\n");
+	printf("Size of a long: "& sizeof(long)&"\n");
+	printf("Size of a long int: "& sizeof(long int)&"\n");
+	printf("Size of a long long int: "& sizeof(long long int)&"\n");
+	printf("Size of a float: "& sizeof(float)&"\n");
+	return (0);
+}
