52431c22771f51c5d50fdf4926a0eedf9a4c92f4
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 07:13:48 -0700

    commited

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Prints upper and lowere case of a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	int k = 0;
+	char alp[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int alp_len = strlen(alp);
+	int alp_upp_lower = alp_len * 2
+
+	do {
+	if ( n < 27 )
+	{
+	putchar(alp(n));
+	}
+	else
+	{
+	putchar(toupper(alp(k));
+	k++
+	}
+	n++
+	} while (n < alp_upp_lower);
+	return (0);
+}
