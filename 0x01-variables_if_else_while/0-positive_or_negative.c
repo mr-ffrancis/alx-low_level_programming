9084d4fe5ee7bb9c9f0105670a0ff04706479232
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 05:14:03 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if(n>0)
 		printf("is positive\n");
-	else if (n==0)
-		printf("is zero\n");
+	else if (n<0)
+		printf("is negative\n");
 	else
-		printf("is negative\n"); 	
+		printf("is zero\n"); 	
 	return (0);
 }
