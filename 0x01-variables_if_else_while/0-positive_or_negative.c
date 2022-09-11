f5a15581e473a193603c45b26856e2e2199ea002
mr-ffrancis <adiotunji@gmail.com>
2022-09-11 05:10:51 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdlib.h> 
+#include <stdio.h>
 /**main function starts the code
 *assigns a randon number to variable n
 *check if the number is positive or negative and prints the result
