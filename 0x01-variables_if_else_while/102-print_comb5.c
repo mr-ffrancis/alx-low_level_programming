c68714b9fcde3302dddeb46a6095a57c3244c36e
mr-ffrancis <adiotunji@gmail.com>
2022-09-12 16:34:20 -0700

    commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -33,9 +33,12 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
+						if (h == 57 && g == 56)
 						{
-							break;
+							if (e == 57 && i == 57)
+							{
+								break;
+							}
 						}
 						putchar(',');
 						putchar(' ');
