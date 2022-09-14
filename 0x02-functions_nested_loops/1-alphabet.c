bded5cffa7bd62aa5e2eedc5a71e236095156eed
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 11:49:53 -0700

    commited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - Entry Point
+ *Print Alphabet
+ *Return: 0 on sucess
+ *
+ */
+
+
+void print_alphabet(void)
+{
+	for (int a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	return (0);
+}
