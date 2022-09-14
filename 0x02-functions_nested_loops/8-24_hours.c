12256fb197c1cc953f67281eaf61edbe1e49c283
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 14:18:18 -0700

    commited

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (b = 48; b <= 52; b++)
 		{
 			for (c = 48; c <= 57; c++)
 			{
