36775f55409d8bf5d78e489270adfdd84407fe00
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 13:19:20 -0700

    commited

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - Entry Point
+ *@n:is an acii alpabetical parameter
+ *Return: 0 on sucess
+ *
+ */
+int print_last_digit(int n)
+{
+	in l = n  % 10;
+	return (l);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 int _isalpha(int c);
 int _putchar(char);
 void print_alphabet(void);
