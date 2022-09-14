4b4e34da3eacb0f2c45a76bef1c724b10e6ecd9d
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 15:08:21 -0700

    commited

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Entry Point
+ *@n:is an acii alpabetical parameter
+ *Return: 0 on sucess
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int print_sign(int n);
 int _abs(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 int print_last_digit(int);
 int _isalpha(int c);
 int _putchar(char);
