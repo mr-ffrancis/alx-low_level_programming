e00eea7b40e097cdb8c990708d09aa87adf21212
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 15:00:50 -0700

    commited

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *
+ * times_table - Entry Point
+ *Print Alphabet
+ *Return: 0 on sucess
+ *
+ */
+
+void times_table(void)
+{
+	int a, b, ops;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			ops = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (ops <= 9)
+			{
+				_putchar(32);
+				_putchar(ops + 48);
+			}
+			else
+			{
+				_putchar((ops / 10) + 48);
+				_putchar((ops % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 int print_sign(int n);
 int _abs(int);
 void jack_bauer(void);
+void times_table(void);
 int print_last_digit(int);
 int _isalpha(int c);
 int _putchar(char);
