c2f78f3a69043a8dc9c0844bcfb3fa952293c9b2
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 15:27:20 -0700

    commited

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_to_98 - Entry Point
+ *
+ *@n: value
+ *Print Alphabet
+ *Return: 0 on sucess
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{                    
+	       for (; n > 98; n++)
+	       {
+		       printf("%d, ", n);
+	       }
+	}
+	printf("%d\n", n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _abs(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 int print_last_digit(int);
 int _isalpha(int c);
 int _putchar(char);
