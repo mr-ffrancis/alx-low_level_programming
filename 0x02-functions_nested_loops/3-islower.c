059b59d98fc3f5a317f77dd8a4bd0393ad29c9a8
mr-ffrancis <adiotunji@gmail.com>
2022-09-14 12:35:37 -0700

    commited

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *_islower - Entry Point
- *
+ *@c: is an acii alpabetical parameter 
  *Return: 0 on sucess
  */
 int _islower(int c)
-{	
+{
 	if (c > 97 && c <= 122)
 		return (1);
 	else
