5d352f8e84201d27c1bdaafee62df5a08db90a52
mr-ffrancis <adiotunji@gmail.com>
2022-09-15 13:29:34 -0700

    commited

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,12 +1,12 @@
-#include "main.h"
 #include <stdio.h>
 /**
  *
- * * main - causes an infinite loop
+ * main - causes an infinite loop
+ * Return: 0
  *
- * * Return: 0
  *
- * */
+ */
+
 int main(void)
 {
 	int i;
