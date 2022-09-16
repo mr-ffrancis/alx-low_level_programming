6f7e582dee7777854d393264129c63f7b009b828
mr-ffrancis <adiotunji@gmail.com>
2022-09-16 15:34:16 -0700

    commited

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,16 @@
  */
 void more_numbers(void)
 {
-	int a, b, c;
+	int b, c;
 
 	for (c = 0; c < 10; c++)
 	{
 		for (b = 0; b < 15; b++)
 		{
 			if (b > 10)
-				_putchar((b / 10) +48);
+			{
+				_putchar((b / 10) + 48);
+			}
 			_putchar((b % 10) + 48);
 		}
 		_putchar('\n');
