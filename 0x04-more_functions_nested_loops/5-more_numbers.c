79392c0c7e557529b904150fd69012b6ae05bef9
mr-ffrancis <adiotunji@gmail.com>
2022-09-16 15:00:39 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers -Entry point
+ * Return: Number 0 -14
+ */
+
+void more_numbers(void)
+{
+	for (int a = 48; a <= 57; a++)
+	{
+		for (int b = 48; b <= 57; b++)
+		{
+			if (a < 49)
+				_putchar(b);
+			else if (a == 49 && b < 53)
+			{
+				_putchar(a);
+				_putchar(b);
+			}
+			else if (a > 49)
+				break;
+		}
+	}
+	_putchar('\n');
+}
