d9bcf97c8313e2b7eedbe875fe760db9599067a8
mr-ffrancis <adiotunji@gmail.com>
2022-09-16 14:05:41 -0700

    commited

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,11 @@
  * print_numbers - Entry Point
  * Return: 0 on sucess
  */
-int print_numbers(void)
+void print_numbers(void)
 {
 	for (int a = 48; a < 58; a++)
 	{
-		_putchar('%c', a);
+		_putchar("%c", a);
 	}
 	_putchar('\n');
 }
