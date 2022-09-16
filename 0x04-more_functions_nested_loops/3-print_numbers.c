6040f28592cd468d117b7d6f76f5e508e4e6c726
mr-ffrancis <adiotunji@gmail.com>
2022-09-16 13:46:51 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Entry Point
+ * Return: 0 on sucess
+ */
+void print_numbers(void)
+{
+	for (int a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	return (0);
+}
