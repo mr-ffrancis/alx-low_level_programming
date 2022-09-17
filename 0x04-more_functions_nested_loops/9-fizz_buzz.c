8a11f434b9cd3cb665c6ba2a9520b46777599ed5
mr-ffrancis <adiotunji@gmail.com>
2022-09-16 17:12:04 -0700

    commited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return fizz buzz
+ * Return:0 on sucess
  */
 int main(void)
 {
 	int i = 1;
 
 	do {
-		if ( i % 5 == 0 || i % 3 == 0)
+		if (i % 5 == 0 || i % 3 == 0)
+		{
 			if (i % 5 == 0 && i % 3 == 0)
+			{
 				printf("FizzBuzz ");
+			}
 			else if (i % 5 == 0 || i % 3 != 0)
+			{
 				printf("Buzz ");
+			}
 			else
+			{
 				printf("Fizz ");
+			}
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 		i++;
 	} while (i <= 100);
+	return (0);
 }
