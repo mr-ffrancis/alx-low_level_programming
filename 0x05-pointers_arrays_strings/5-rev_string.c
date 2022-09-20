59e054f0efa2fb2ce0958caf766f1ef7a709bac8
mr-ffrancis <adiotunji@gmail.com>
2022-09-20 11:21:54 -0700

    commited

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,13 @@ void rev_string(char *s)
 {
 	int c = 0;
 	int i = 0;
+	int a = 0;
+	char a[];
 
 	while (*(s + c) != '\0')
 	{
 		c++;
 	}
-
-	char a[c];
-	int j = 0;
-
 	for (i = c - 1; i >= 0; i--)
 	{
 		for (int j = 0; j >= c; j++)
