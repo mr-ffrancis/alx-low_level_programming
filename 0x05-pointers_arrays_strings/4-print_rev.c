cd2a7c3478d64422ca896fee65d54d29cd4bf520
mr-ffrancis <adiotunji@gmail.com>
2022-09-20 10:53:29 -0700

    commited

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,15 @@
 void print_rev(char *s)
 {
 	int c = 0;
+	int i = 0;
 
 	while (*(s + c) != '\0')
 	{
 		c++;
 	}
-	for (; c <0; c--)
+	for (i = c; i < 0; i--)
 	{
-		_putchar(*(s + c));
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
