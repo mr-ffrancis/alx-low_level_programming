fd0db502d6024692964402a2f5e828df1f07a1fa
mr-ffrancis <adiotunji@gmail.com>
2022-10-10 10:24:54 -0700

    commited

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+
+/*
+ * struct dog - struct that stores some information of a dog
+ * @name and the name of dog
+ * @age age of dog
+ * @owner name of owner
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+#endif
