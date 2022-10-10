ba526cb647bd9e6203c680e8e7cde65c6597761e
mr-ffrancis <adiotunji@gmail.com>
2022-10-10 11:41:15 -0700

    commited

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (lname = 0; name[lname]; lname++)
-		:
+		;
 	for (lowner = 0; owner[lowner]; lowner++)
 		;
 	p_dog->name = malloc(lname + 1);
