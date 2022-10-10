94eaa53d2b26c0c4d32e0336a2f4a5e56d5440bf
mr-ffrancis <adiotunji@gmail.com>
2022-10-10 11:18:21 -0700

    commited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define _DOG_
 
 /**
- * dog_h - Typedef for dog
+ * dog_t - Typedef for dog
  * struct dog - stucture that describes the dog.
  * @name: and the name of dog
  * @age: age of dog
  * @owner: name of owner
  */
-typedef struct dog dog_h;
 struct dog
 {
 	char *name;
@@ -16,6 +15,7 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
